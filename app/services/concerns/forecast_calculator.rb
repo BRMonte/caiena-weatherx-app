d91855d6a67ac48f0d6aa0358f64ec8a11298335
Bruno Montebello <51917604+BRMonte@users.noreply.github.com>
2025-09-30 22:40:50 +0100

    Create test + module for calculating temperature averages

---
+++ app/services/concerns/forecast_calculator.rb
@@ -0,0 +1,33 @@
+module ForecastCalculator
+  extend ActiveSupport::Concern
+
+  def calculate_daily_averages(forecast_list)
+    grouped_by_date = group_forecasts_by_date(forecast_list)
+    
+    tomorrow = Date.current + 1.day
+    future_forecasts = grouped_by_date.select do |date, forecasts|
+      Date.parse(date) >= tomorrow
+    end
+    
+    future_forecasts.map do |date, forecasts|
+      {
+        date: date,
+        average_temperature: calculate_average_temperature(forecasts)
+      }
+    end.first(5)
+  end
+  
+  private
+  
+  def group_forecasts_by_date(forecast_list)
+    forecast_list.group_by do |forecast|
+      Date.parse(forecast['dt_txt']).strftime('%Y-%m-%d')
+    end
+  end
+  
+  def calculate_average_temperature(forecasts)
+    temperatures = forecasts.map { |f| f['main']['temp'] }
+    average_kelvin = temperatures.sum / temperatures.count
+    (average_kelvin - 273.15).round(2)
+  end
+end
